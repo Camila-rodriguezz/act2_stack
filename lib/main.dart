316da6255344c3927832e74af98edf2a09513ebd
CAMILA_RODRIGUEZ_RUIZ <a.22308051281300@cbtis128.edu.mx>
2025-03-20 20:07:10 +0000

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,103 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
+
+class Empleado {
+  final String idEmpleado;
+  final String nombre;
+  final String puesto;
+
+  Empleado({
+    required this.idEmpleado,
+    required this.nombre,
+    required this.puesto,
+  });
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+  final Empleado empleado = Empleado(
+    idEmpleado: '001',
+    nombre: 'Juan Pérez',
+    puesto: 'Desarrollador',
+  );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Camila Rodriguez Ruiz 1300'),
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  // Contenedor con estilos
+                  Container(
+                    width: 300,
+                    height: 200,
+                    decoration: BoxDecoration(
+                      color: Colors.blue[100],
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black26,
+                          blurRadius: 10,
+                          offset: Offset(0, 5),
+                        )
+                      ],
+                      border: Border.all(
+                        color: Colors.blueAccent,
+                        width: 2,
+                      ),
+                      gradient: LinearGradient(
+                        colors: [Colors.blue[100]!, Colors.blue[200]!],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                      ),
+                    ),
+                  ),
+                  // Contenido del Stack
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        'ID: ${empleado.idEmpleado}',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue[900],
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Nombre: ${empleado.nombre}',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.blue[800],
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Puesto: ${empleado.puesto}',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.blue[800],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
